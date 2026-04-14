616cc38eacff4f1335931aa46f860093e8e535f2
CarloBu <karolis.butvinskas@gmail.com>
2026-04-14 10:53:35 +0300

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-page-tree/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-page-tree/rsc'
+
+export const importMap = {
+  'payload-page-tree/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-page-tree/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
